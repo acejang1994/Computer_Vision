2721a0dac34362a6d76edac7bd7bd14fa3fe86b1
flymperopoulos <filippos.lymperopoulos@students.olin.edu>
2015-10-29 00:17:41 -0400

    ADD: path contour track

--- scripts/path.py
+++ scripts/path.py
@@ -23,22 +23,52 @@ roi =  mask[height-240:height -160,0:width]
 roi2 = mask [height-160:height-80,0:width]
 roi3 = mask [height-80:height,0:width]
 
+contours, hierarchy = cv2.findContours(roi,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours2, hierarchy = cv2.findContours(roi2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours3, hierarchy = cv2.findContours(roi3,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-print roi
-cv2.imshow("roi", roi)
-cv2.imshow("roi2", roi2)
-cv2.imshow("roi3", roi3)
+for i in contours:
+	area = cv2.contourArea(i)
+	moments = cv2.moments (i)
+
+	if moments['m00']!=0.0:
+		if moments['m01']!=0.0:
+			cx = int(moments['m10']/moments['m00'])         # cx = M10/M00
+    		cy = int(moments['m01']/moments['m00'])         # cy = M01/M00
+    		cv2.circle(roi,(cx,cy), 4, (255,0,0), -1)
+    		cv2.circle(roi,(cx,cy), 8, (0,255,0), 0)
+    		# cv2.rectangle(mask,(x,y+80),(x+w,y+h+80),(0,255,0),2)
+
+for i in contours2:
+	area = cv2.contourArea(i)
+	moments = cv2.moments (i)
+
+	if moments['m00']!=0.0:
+		if moments['m01']!=0.0:
+			cx2 = int(moments['m10']/moments['m00'])         # cx = M10/M00
+    		cy2 = int(moments['m01']/moments['m00'])         # cy = M01/M00
+    		cv2.circle(roi2,(cx2,cy2), 4, (255,0,0), -1)
+    		cv2.circle(roi2,(cx2,cy2), 8, (0,255,0), 0)
+    		# cv2.rectangle(roi2,(x,y+80),(x+w,y+h+80),(0,255,0),2)
+for i in contours3:
+	area = cv2.contourArea(i)
+	moments = cv2.moments (i)
+
+	if moments['m00']!=0.0:
+		if moments['m01']!=0.0:
+			cx3 = int(moments['m10']/moments['m00'])         # cx = M10/M00
+    		cy3 = int(moments['m01']/moments['m00'])         # cy = M01/M00
+    		cv2.circle(roi3,(cx3,cy3), 4, (255,0,0), -1)
+    		cv2.circle(roi3,(cx3,cy3), 8, (0,255,0), 0)
 
 cv2.imshow("mask", mask)
-(cnts, _) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-center_x , center_y = 0, 0
 
-moments = cv2.moments(mask)
-if moments['m00'] != 0:
-    center_x, center_y = moments['m10']/moments['m00'], moments['m01']/moments['m00']
-print center_x, center_x
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+cv2.line(mask, (cx,cy), (cx2,cy2), (0,255,0), 2)
+cv2.line(mask, (cx2,cy2), (cx3,cy3), (0,255,0), 2)
 
+# cv2.imshow("roi", roi)
+# cv2.imshow("roi2", roi2)
+# cv2.imshow("roi3", roi3)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
